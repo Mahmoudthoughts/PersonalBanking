5143a9bd7503439554343f8e889dd109e1af3c6c
Mahmoudthoughts <mahibrahim@outlook.com>
2025-07-20 17:45:15 +0400

    feat(frontend): configure API base URL

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
   constructor(private http: HttpClient) {}
 
   getTransactions() {
-    return this.http.get<any[]>('/transactions');
+    return this.http.get<any[]>(`${environment.apiUrl}/transactions`);
   }
 }
