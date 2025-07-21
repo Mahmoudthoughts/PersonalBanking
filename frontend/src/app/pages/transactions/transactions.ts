a041cbfb18c583ccd91f7a01897c1bce215be247
Mahmoudthoughts <mahibrahim@outlook.com>
2025-07-21 18:35:15 +0400

    Implement transaction filters and search form

--- frontend/src/app/pages/transactions/transactions.ts
+++ frontend/src/app/pages/transactions/transactions.ts
@@ -1,23 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './transactions.html',
   styleUrl: './transactions.scss'
 })
 export class Transactions implements OnInit {
   transactions: any[] = [];
+  filters = {
+    amount: '',
+    tag: '',
+    cardholder: '',
+    desc: '',
+    start: '',
+    end: ''
+  };
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<any[]>(`${environment.apiUrl}/transactions`).subscribe(res => {
-      this.transactions = res;
-    });
+    this.load();
+  }
+
+  load() {
+    let params = new HttpParams();
+    for (const [key, value] of Object.entries(this.filters)) {
+      if (value) {
+        params = params.set(key, value as string);
+      }
+    }
+    this.http
+      .get<any[]>(`${environment.apiUrl}/transactions`, { params })
+      .subscribe(res => {
+        this.transactions = res;
+      });
   }
 }
